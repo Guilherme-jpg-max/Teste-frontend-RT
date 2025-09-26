f97608a860679112636d60606a6dd9891e1f1435
Guilherme-jpg-max <guicasii5@gmail.com>
2025-09-26 01:30:20 -0300

    externaliza URL da API para variáveis de ambiente

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://beta.guardia-api.box3.work/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 export default api;
